3ace9f16d4cae78dbd3ac052ed88daa6c98d1559
ASUS <ASUS@LAPTOP-3N47BETR>
2020-06-07 14:43:29 +0300

    added the connect button and the enter url text box.

---
+++ app/src/main/java/com/example/flightmobileapp/GameActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flightmobileapp
+
+class GameActivity {
+
+}
--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.flightmobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val connectButton = findViewById<Button>(R.id.connectButton)
+        connectButton.setOnClickListener{
+            val input = typeUrl.text
+            Toast.makeText(this, input, Toast.LENGTH_LONG).show()
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
